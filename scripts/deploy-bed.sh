320fdf14bf02d57a75ceaff2f891300e91280a63
nobisImperium <49238200+nobisImperium@users.noreply.github.com>
2019-07-23 09:51:38 -0500

    Update deploy-bed.sh

--- scripts/deploy-bed.sh
+++ scripts/deploy-bed.sh
@@ -1,8 +1,5 @@
-#0 Script initialization
+# 0 Script initialization
 ### Static Steelscript Vars
 linuxos="redhat-release"
-############################################################################################
-# 1 Install all platform prerequisites: Essentially a LAMP Stack and Development Tools
-	# Base Packages System/Wide/Libs/etc
-	sudo yum clean all
+sudo yum clean all
 exit 0
